2be044c2115440b112e437812ef6f85e2a696d80
MrTulok <tulokvendel@gmail.com>
2024-03-14 15:12:18 +0100

    gyakorlas5

--- gyak5/Form1.Designer.cs
+++ gyak5/Form1.Designer.cs
@@ -28,19 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 406);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(796, 144);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(67, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 32);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(555, 96);
+            textBox1.TabIndex = 2;
+            textBox1.Click += textBox1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 134);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(555, 96);
+            textBox2.TabIndex = 3;
+            textBox2.Click += textBox2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 251);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(555, 96);
+            textBox3.TabIndex = 4;
+            textBox3.Click += textBox3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(634, 74);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(418, 309);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(850, 428);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 65);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1073, 552);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
--- gyak5/Form1.cs
+++ gyak5/Form1.cs
@@ -2,6 +2,9 @@ namespace gyak5
 {
     public partial class Form1 : Form
     {
+        List<Kérdés> ÖsszesKérdés;
+        List<Kérdés> AktuálisKérdés = new List<Kérdés>();
+        int MegjelenítettKérdésSzáma = 5;
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +12,124 @@ namespace gyak5
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ÖsszesKérdés = KérdésekBeolvasása();
+            for (int i = 0; i < 7; i++)
+            {
+                AktuálisKérdés.Add(ÖsszesKérdés[0]);
+                ÖsszesKérdés.RemoveAt(0);
+            }
+            dataGridView1.DataSource = AktuálisKérdés;
 
+            KérdésMegjelenítése(AktuálisKérdés[MegjelenítettKérdésSzáma]);
+        }
+
+        void KérdésMegjelenítése(Kérdés kérdés)
+        {
+            label1.Text = kérdés.KérdésSzöveg;
+            textBox1.Text = kérdés.Válasz1;
+            textBox2.Text = kérdés.Válasz2;
+            textBox3.Text = kérdés.Válasz3;
+
+            if (string.IsNullOrEmpty(kérdés.URL))
+            {
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Load("https://storage.altinum.hu/hajo/" + kérdés.URL);
+            }
+
+
+            textBox1.BackColor = Color.White;
+            textBox2 .BackColor = Color.White;
+            textBox3 .BackColor = Color.White;
+
+        }
+
+        List<Kérdés> KérdésekBeolvasása()
+        {
+            List<Kérdés> kérdések = new();
+            StreamReader sr = new StreamReader("hajozasi_szabalyzat_kerdessor_BOM.txt", true);
+
+            while (!sr.EndOfStream)
+            {
+                string? sor = sr.ReadLine()/*vagy ?? n/a*/;
+                string[] tömb = sor.Split("\t");
+
+                if (tömb.Length != 7) continue;
+
+                Kérdés k = new();
+
+                k.KérdésSzöveg = tömb[1].ToUpper();
+                k.Válasz1 = tömb[2].Trim('"');
+                k.Válasz2 = tömb[3];
+                k.Válasz3 = tömb[4];
+                k.URL = tömb[5];
+
+                int x = 0;
+                int.TryParse(tömb[6], out x);
+
+                k.HelyesVálasz = x;
+
+                kérdések.Add(k);
+            }
+
+            sr.Close();
+            return kérdések;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MegjelenítettKérdésSzáma++;
+            if (MegjelenítettKérdésSzáma == AktuálisKérdés.Count) MegjelenítettKérdésSzáma = 0;
+
+            KérdésMegjelenítése(AktuálisKérdés[MegjelenítettKérdésSzáma]);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            
+            if (AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálasz == 1)
+            {
+                textBox1.BackColor = Color.LightGreen;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma++;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Salmon;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma = 0;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            
+            if (AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálasz == 2)
+            {
+                textBox2.BackColor = Color.LightGreen;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma++;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Salmon;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma = 0;
+            }
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            
+            if (AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálasz == 3)
+            {
+                textBox3.BackColor = Color.LightGreen;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma++;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Salmon;
+                AktuálisKérdés[MegjelenítettKérdésSzáma].HelyesVálaszokSzáma = 0;
+            }
         }
     }
 }
